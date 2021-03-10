114b89ddbb530133bd721f3917ce4a5b53628e84
DavidZolotoi <zolotoi77@inbox.ru>
2021-03-11 01:18:17 +0300

    Сделал домашнюю работу №2

--- lessons/02/Homework/Homework2/Homework2/Program.cs
+++ lessons/02/Homework/Homework2/Homework2/Program.cs
@@ -6,7 +6,45 @@ namespace Homework2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string deistvie;
+            double chislo1, chislo2;
+
+            Console.Write("Выберите действие (+, -, *, /, %, ^):");
+            deistvie = Console.ReadLine();                  //стоить сделать проверку значения
+
+            Console.Write("Введите первое число:");
+            chislo1 = double.Parse(Console.ReadLine());     //стоить сделать проверку значения
+
+            Console.Write("Введите второе число:");
+            chislo2 = double.Parse(Console.ReadLine());     //стоить сделать проверку значения
+
+            Console.Write(chislo1 + deistvie + chislo2 + "=");
+
+            switch (deistvie)
+            {
+                case "+":
+                    Console.WriteLine(chislo1 + chislo2);
+                    break;
+                case "-":
+                    Console.WriteLine(chislo1 - chislo2);
+                    break;
+                case "*":
+                    Console.WriteLine(chislo1 * chislo2);
+                    break;
+                case "/":
+                    if (chislo2 == 0) Console.WriteLine("На ноль делить нельзя!"); else Console.WriteLine(chislo1 / chislo2);
+                    break;
+                case "%":
+                    if (chislo2 == 0) Console.WriteLine("На ноль делить нельзя!"); else Console.WriteLine(chislo1 % chislo2);
+                    break;
+                case "^":
+                    Console.WriteLine(Math.Pow(chislo1, chislo2));
+                    break;
+                default:
+                    Console.WriteLine("Не удалось посчитать, вероятно введенные данные неверны.");
+                    break;
+            }
+
         }
     }
 }
