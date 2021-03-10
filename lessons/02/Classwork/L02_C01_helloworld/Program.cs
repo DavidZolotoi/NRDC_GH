44ccd1f44e97a940dbb12263292ea05a46fde08f
DavidZolotoi <zolotoi77@inbox.ru>
2021-03-11 01:15:27 +0300

    Распаковал классную работу №2

---
+++ lessons/02/Classwork/L02_C01_helloworld/Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace L02_C01_helloworld
+{
+	class Program
+	{
+		// start of external block
+		static void Main()
+		{
+			Console.WriteLine("Enter two numbers: ");
+			double left = double.Parse(Console.ReadLine());
+			double right = double.Parse(Console.ReadLine());
+			Console.WriteLine(left + right);
+
+			Boolean condition = false;
+			if (!condition)
+			{
+			}
+
+			int flag = 1;
+			if (flag !>1)
+			{
+			}
+		}
+		
+	}
+} // end of external block
