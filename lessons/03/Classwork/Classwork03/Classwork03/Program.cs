bfab65038916c42677fdd235d97c747f0b71bafc
DavidZolotoi <zolotoi77@inbox.ru>
2021-03-11 22:14:06 +0300

    Классная 3его занятия

---
+++ lessons/03/Classwork/Classwork03/Classwork03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Classwork03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.
+            dynamic din;
+            din = "stroka";
+            din = 77;
+            Console.WriteLine(din*din);
+
+            // 2.
+            object ob;
+            ob = "stroka";
+            Console.WriteLine(ob.ToString().Length);
+
+            // 3.
+            var per1=1;
+            var per2=2;
+            var per3=3;
+
+            // 4.
+            var stroki = new string[4];
+            for (var i=0; i<stroki.Length; i++)
+            {
+                stroki[i]=Console.ReadLine();
+            }
+
+        }
+    }
+}
