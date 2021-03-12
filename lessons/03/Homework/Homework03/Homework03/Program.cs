cd92d785e8fa83f4d4071ec967b396a7d55cd5ba
David Zolotoi <zolotoi77@inbox.ru>
2021-03-12 16:27:28 +0300

    Сделал домашнее задание 3-го занятия

--- lessons/03/Homework/Homework03/Homework03/Program.cs
+++ lessons/03/Homework/Homework03/Homework03/Program.cs
@@ -6,7 +6,31 @@ namespace Homework03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите количество людей: ");
+            int kolvoImen = int.Parse(Console.ReadLine());
+            Console.Write("Сколько лет добавить? ");
+            int delta = int.Parse(Console.ReadLine());
+
+            var imya = new string [kolvoImen];
+            var vozrast1 = new int[kolvoImen];
+            var vozrast2 = new int[kolvoImen];
+
+            for (int i = 0; i < kolvoImen; i++)
+            {
+                Console.Write($"\nИмя {i+1}-о человека: ");
+                imya[i]=Console.ReadLine();
+                Console.Write($"Его возраст: ");
+                vozrast1[i] = int.Parse(Console.ReadLine());
+
+                vozrast2[i] = vozrast1[i] + delta;
+            }
+
+            for (int i=0; i<kolvoImen; i++)
+            {
+                Console.Write($"\n{i+1}. Имя: {imya[i]}, возраст через {delta} лет (год, года): {vozrast2[i]}");
+            }
+
+
         }
     }
 }
