e249f0258a99a452e80e2f06bd44cc5adce10c84
David Zolotoi <zolotoi77@inbox.ru>
2021-03-12 13:04:40 +0300

    Дал названия в комментариях

--- lessons/03/Classwork/Classwork03/Classwork03/Program.cs
+++ lessons/03/Classwork/Classwork03/Classwork03/Program.cs
@@ -6,23 +6,23 @@ namespace Classwork03
     {
         static void Main(string[] args)
         {
-            // 1.
+            // 1. Работа с переменными dynamic
             dynamic din;
             din = "stroka";
             din = 77;
             Console.WriteLine(din*din);
 
-            // 2.
+            // 2. Работа с object
             object ob;
             ob = "stroka";
             Console.WriteLine(ob.ToString().Length);
 
-            // 3.
+            // 3. Работа с var
             var per1=1;
             var per2=2;
             var per3=3;
 
-            // 4.
+            // 4. Работа со строковыми массивами
             var stroki = new string[4];
             for (var i=0; i<stroki.Length; i++)
             {
