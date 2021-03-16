e700037f4d78caaa4104688ace4dbf7914d4e05e
DavidZolotoi <zolotoi77@inbox.ru>
2021-03-16 22:31:39 +0300

    конец 3 классной работы

--- lessons/04/Classwork/Classwork04/Classwork04/Program.cs
+++ lessons/04/Classwork/Classwork04/Classwork04/Program.cs
@@ -18,6 +18,9 @@ namespace Classwork04
             Console.WriteLine($"Боковая площ. Sб = {Math.Round(3*a*h, 3, MidpointRounding.AwayFromZero)}");
             Console.WriteLine($"Полная площадь Sп = {Math.Round((3.0/2)*a*(a*Math.Sqrt(3)+2*h), 3, MidpointRounding.AwayFromZero)}");
             Console.WriteLine($"Объем пирамиды V = {Math.Round(a*a*H*Math.Sqrt(3)/2, 3, MidpointRounding.AwayFromZero)}");
+
+
+
         }
     }
 }
