564878abeeff2ccd8bee7b2f0a0c4f38872f3738
DavidZolotoi <zolotoi77@inbox.ru>
2021-03-16 21:14:45 +0300

    1ая самостоялка про расчет параметров пирамиды

--- lessons/04/Classwork/Classwork04/Classwork04/Program.cs
+++ lessons/04/Classwork/Classwork04/Classwork04/Program.cs
@@ -6,7 +6,18 @@ namespace Classwork04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // пирамида
+            Console.Write("Введите сторону основания а:");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите высоту h:");
+            double h = double.Parse(Console.ReadLine());
+
+            var H = Math.Round(Math.Sqrt(h * h - a * a / 12), 3, MidpointRounding.AwayFromZero);
+
+            Console.WriteLine($"Высота пирамиды H = {H}");
+            Console.WriteLine($"Боковая площ. Sб = {Math.Round(3*a*h, 3, MidpointRounding.AwayFromZero)}");
+            Console.WriteLine($"Полная площадь Sп = {Math.Round((3.0/2)*a*(a*Math.Sqrt(3)+2*h), 3, MidpointRounding.AwayFromZero)}");
+            Console.WriteLine($"Объем пирамиды V = {Math.Round(a*a*H*Math.Sqrt(3)/2, 3, MidpointRounding.AwayFromZero)}");
         }
     }
 }
