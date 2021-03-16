f49741f5c8cff19236e29d17728a2d2956c83673
DavidZolotoi <zolotoi77@inbox.ru>
2021-03-16 19:15:48 +0300

    Создал проект классной работы №4

---
+++ lessons/04/Classwork/Classwork04/Classwork04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classwork04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
