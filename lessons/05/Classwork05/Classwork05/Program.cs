6771d7b38d2c06e993ecf753c4f6bb385de97ee2
DavidZolotoi <zolotoi77@inbox.ru>
2021-03-18 22:18:14 +0300

    Классная работа № 05

---
+++ lessons/05/Classwork05/Classwork05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Classwork05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.
+            Console.WriteLine("Введите возраст человека:");
+
+            for (int i = 1; i < 10; i++)
+            {
+                var age = byte.Parse(Console.ReadLine());
+                var ost = age % 10;
+
+                var god = ost == 1 && (age >= 12 || age == 1)
+                    ? "год"
+                    : (ost >= 2 && ost <= 4 && (age >= 15 || age < 10))
+                        ? "года"
+                        : "лет";
+
+                Console.WriteLine($"Человеку {age} {god}.");
+
+            }
+
+            // Правильнее найти отдельно десятки и отдельно остатки
+
+        }
+    }
+}
