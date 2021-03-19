fa1d011b133c20e7c4ff2fe69b165114f47da512
David Zolotoi <zolotoi77@inbox.ru>
2021-03-19 16:42:30 +0300

    Домашняя 04 - всё еще не доделана

--- lessons/04/Homework/Homework04/Homework04/Program.cs
+++ lessons/04/Homework/Homework04/Homework04/Program.cs
@@ -8,41 +8,53 @@ namespace Homework04
         [Flags]
         enum ColorAll : short   // 2 байта => 16 бит => 16 вариантов допускается
         {
-            //NoneColor,      //0000_0000_0000_0000  -  0      - 0
-            Black=1 ,       //0000_0000_0000_0001  -  1      - 1
-            Blue    ,       //0000_0000_0000_0010  -  2      - 2
-            Cyan    ,       //0000_0000_0000_0100  -  4      - 3
-            Grey    ,       //0000_0000_0000_1000  -  8      - 4
-            Green   ,       //0000_0000_0001_0000  -  16     - 5
-            Magenta ,       //0000_0000_0010_0000  -  32     - 6
-            Red     ,       //0000_0000_0100_0000  -  64     - 7
-            White   ,       //0000_0000_1000_0000  -  128    - 8
-            Yellow          //0000_0001_0000_0000  -  256    - 9
-        };                               
+            N0_NoneColor = 0x0     ,       //0000_0000_0000_0000  -    0    - 0
+            N1_Black     = 0x1     ,       //0000_0000_0000_0001  -    1    - 1
+            N2_Blue      = 0x1 << 1,       //0000_0000_0000_0010  -    2    - 2
+            N3_Cyan      = 0x1 << 2,       //0000_0000_0000_0100  -    4    - 3
+            N4_Grey      = 0x1 << 3,       //0000_0000_0000_1000  -    8    - 4
+            N5_Green     = 0x1 << 4,       //0000_0000_0001_0000  -   16    - 5
+            N6_Magenta   = 0x1 << 5,       //0000_0000_0010_0000  -   32    - 6
+            N7_Red       = 0x1 << 6,       //0000_0000_0100_0000  -   64    - 7
+            N8_White     = 0x1 << 7,       //0000_0000_1000_0000  -  128    - 8
+            N9_Yellow    = 0x1 << 8        //0000_0001_0000_0000  -  256    - 9
+        };
 
         static void Main(string[] args)
         {
             // Приветствие
-            Console.WriteLine("Выберите цвета из палитры для добавления в 'Избранное':");
-            for (int i=0; i<9; i++) Console.WriteLine($"Цвет №{i+1} - {(ColorAll)(i+1)},");
-            
+             Console.WriteLine("Выберите цвета из палитры для добавления в 'Избранное':");
+
+            // Через while и игру с битами
+            ColorAll a =ColorAll.N1_Black;
+            while (a <= ColorAll.N9_Yellow)
+            {
+                Console.WriteLine($"Цвет {a},");
+                a = (ColorAll)((short)a << 1);
+                Console.WriteLine(ColorAll.N9_Yellow);
+            }
+
             // Выбор цветов
-            var colorFavorites = new byte[4];
-            bool colorParsResult;
+            var colorFavorites = new short[4];
+            var b = ColorAll.N0_NoneColor;
 
             for (int j = 0; j < 4; j++)
             {
                 Console.Write($"Добавить цвет №");
                 try
                 {
-                    colorParsResult = byte.TryParse(Console.ReadLine(), out colorFavorites[j]);
-
+                    // colorParsResult = short.TryParse(Console.ReadLine(), out colorFavorites[j]);
+                    colorFavorites[j] = short.Parse(Console.ReadLine());
+                    b = b | (ColorAll)((short)colorFavorites[j]);
+                    Console.WriteLine(b);
                 }
                 catch
                 {
                     Console.WriteLine($"Не удалось выбрать цвет, возможно введены некорректные данные.");
                 }
             }
+
+            Console.WriteLine($"b = {b}");
 
             // Вывод цветов
             Console.WriteLine("Список цветов, добавленных в 'Избранное':");
@@ -52,9 +64,9 @@ namespace Homework04
                 //Console.WriteLine($"{(ColorAll)((byte)ColorAll.Black << (colorFavorites[k]-1))}");
             }
 
-            Console.WriteLine($"{ColorAll.Blue | ColorAll.Cyan}");
+            // Console.WriteLine($"{ColorAll.N2_Blue | ColorAll.N3_Cyan}");
             // эксперимент
-            Console.WriteLine($"{(ColorAll)((byte)ColorAll.Black << 2)}");
+            // Console.WriteLine($"{(ColorAll)((short)ColorAll.N1_Black << 2)}");
 
         }
     }
