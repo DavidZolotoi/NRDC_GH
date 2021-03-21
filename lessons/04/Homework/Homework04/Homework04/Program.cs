5193d7bb987162220f00454d4383107354ca2777
DavidZolotoi <zolotoi77@inbox.ru>
2021-03-21 17:01:34 +0300

    Сделал домашнее задание №4

--- lessons/04/Homework/Homework04/Homework04/Program.cs
+++ lessons/04/Homework/Homework04/Homework04/Program.cs
@@ -6,70 +6,68 @@ namespace Homework04
     {
         // Перечисление всех цветов палитры
         [Flags]
-        enum ColorAll : short   // 2 байта => 16 бит => 16 вариантов допускается
+        enum Color : short   // 2 байта => 16 бит => 16 вариантов допускается
         {
-            N0_NoneColor = 0x0     ,       //0000_0000_0000_0000  -    0    - 0
-            N1_Black     = 0x1     ,       //0000_0000_0000_0001  -    1    - 1
-            N2_Blue      = 0x1 << 1,       //0000_0000_0000_0010  -    2    - 2
-            N3_Cyan      = 0x1 << 2,       //0000_0000_0000_0100  -    4    - 3
-            N4_Grey      = 0x1 << 3,       //0000_0000_0000_1000  -    8    - 4
-            N5_Green     = 0x1 << 4,       //0000_0000_0001_0000  -   16    - 5
-            N6_Magenta   = 0x1 << 5,       //0000_0000_0010_0000  -   32    - 6
-            N7_Red       = 0x1 << 6,       //0000_0000_0100_0000  -   64    - 7
-            N8_White     = 0x1 << 7,       //0000_0000_1000_0000  -  128    - 8
-            N9_Yellow    = 0x1 << 8        //0000_0001_0000_0000  -  256    - 9
+            // цвет     в 16-й с.ч.       в 2-й с.ч.             в 10-й    №
+            NoneColor = 0x0     ,       //0000_0000_0000_0000  -    0    - 0
+            Black     = 0x1     ,       //0000_0000_0000_0001  -    1    - 1
+            Blue      = 0x1 << 1,       //0000_0000_0000_0010  -    2    - 2
+            Cyan      = 0x1 << 2,       //0000_0000_0000_0100  -    4    - 3
+            Grey      = 0x1 << 3,       //0000_0000_0000_1000  -    8    - 4
+            Green     = 0x1 << 4,       //0000_0000_0001_0000  -   16    - 5
+            Magenta   = 0x1 << 5,       //0000_0000_0010_0000  -   32    - 6
+            Red       = 0x1 << 6,       //0000_0000_0100_0000  -   64    - 7
+            White     = 0x1 << 7,       //0000_0000_1000_0000  -  128    - 8
+            Yellow    = 0x1 << 8        //0000_0001_0000_0000  -  256    - 9
         };
 
         static void Main(string[] args)
         {
-            // Console.WriteLine(Convert.ToString(0x1 << 2, 2));
-
             // Приветствие
             Console.WriteLine("Выберите цвета из палитры для добавления в 'Избранное':");
 
-            // Через while и игру с битами
-            ColorAll a =ColorAll.N1_Black;
-            while (a <= ColorAll.N9_Yellow)
+            // Выдача списка через игру с битами, for и одну переменную color, которая как итератор, так и выводимое значение
+            // colorAll - это просто сбор всех цветов в одной переменной
+            Color colorAll = Color.NoneColor;
+            for (Color color = Color.Black; color <= Color.Yellow; color = (Color)((short)color << 1))
             {
-                Console.WriteLine($"Цвет {a},");
-                a = (ColorAll)((short)a << 1);
-                Console.WriteLine(ColorAll.N9_Yellow);
+                colorAll |= color;
+                Console.WriteLine($"Цвет: {color},");
             }
 
-            // Выбор цветов
-            var colorFavorites = new short[4];
-            var b = ColorAll.N0_NoneColor;
-
+            // Запись избранного через игру с битами, for и одну переменную
+            Color colorFavorites = Color.NoneColor;
             for (int j = 0; j < 4; j++)
             {
                 Console.Write($"Добавить цвет №");
                 try
                 {
-                    // colorParsResult = short.TryParse(Console.ReadLine(), out colorFavorites[j]);
-                    colorFavorites[j] = short.Parse(Console.ReadLine());
-                    b = b | (ColorAll)((short)colorFavorites[j]);
-                    Console.WriteLine(b);
+                    colorFavorites = (Color)((short)colorFavorites | (short)Math.Pow(2, short.Parse(Console.ReadLine()) - 1));    // т.к. пользователь вводит №, а программа принимает 2^(№-1)
                 }
-                catch
+                catch (Exception ex) when (ex is ArgumentNullException || ex is FormatException || ex is OverflowException)
                 {
-                    Console.WriteLine($"Не удалось выбрать цвет, возможно введены некорректные данные.");
+                    Console.WriteLine("Введены некорректные данные, введите еще раз");
+                    j -= 1;     // уменьшение счётчика, чтоб дать пользователю еще один шанс ввести корректные данные
                 }
             }
 
-            Console.WriteLine($"b = {b}");
+            // Определение списка НЕ избранных цветов
+            Color colorNotFavorites = colorAll ^ colorFavorites;
 
-            // Вывод цветов
-            Console.WriteLine("Список цветов, добавленных в 'Избранное':");
-            for (int k = 0; k < 4; k++)
-            {
-                Console.WriteLine($"Цвет №{colorFavorites[k]} - {(ColorAll)(colorFavorites[k])}");
-                //Console.WriteLine($"{(ColorAll)((byte)ColorAll.Black << (colorFavorites[k]-1))}");
-            }
-
-            // Console.WriteLine($"{ColorAll.N2_Blue | ColorAll.N3_Cyan}");
-            // эксперимент
-            // Console.WriteLine($"{(ColorAll)((short)ColorAll.N1_Black << 2)}");
-
+            // Выдача списков избранных и НЕ... - списков
+            Console.WriteLine($"Список избранных цветов: {colorFavorites}");
+            Console.WriteLine($"Список не избранных цветов: {colorNotFavorites}");
         }
     }
 }
+
+
+// Выдача списка через игру с битами, while и одну переменную color, colorAll - это просто сбор всех цветов в одной переменной
+//Color color = Color.Black,
+//      colorAll = Color.NoneColor;
+//while (color <= Color.Yellow)
+//{
+//    colorAll |= color;
+//    Console.WriteLine($"Цвет: {color},");
+//    color = (Color)((short)color << 1);
+//}
