e8846f3e4e0f10192e0a4e6dd5bf2d864f81d00f
DavidZolotoi <zolotoi77@inbox.ru>
2021-03-31 21:06:07 +0300

    Сделал более грамотной строку if (!char.IsWhiteSpace(s)) pechatnie |= char.IsUpper(s);

--- lessons/07/Homework07/Homework07_2/Program.cs
+++ lessons/07/Homework07/Homework07_2/Program.cs
@@ -61,7 +61,7 @@ namespace Homework07_2
                     // Проверка на наличие печатных букв
                     bool pechatnie=false;
                     foreach (var s in textCR)
-                        if (!char.Equals(s, ' ')) pechatnie |= !char.IsLower(s);
+                        if (!char.IsWhiteSpace(s)) pechatnie |= char.IsUpper(s);
 
                     if (!pechatnie)
                     {
