3cc21f689a72665a1d0c663422db59aa85cb3996
DavidZolotoi <zolotoi77@inbox.ru>
2021-04-01 22:02:02 +0300

    классная 9

---
+++ lessons/09/Classwork09/classwork09/classwork09/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace classwork09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            x = x + 1;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
