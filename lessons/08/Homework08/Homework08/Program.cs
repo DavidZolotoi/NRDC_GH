04dfe545004e3d638a4fc3fa794951233a9ce1ed
DavidZolotoi <zolotoi77@inbox.ru>
2021-04-04 00:03:11 +0300

    Набросал решение ДЗ №08 без обработки исключений и ошибок

---
+++ lessons/08/Homework08/Homework08/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Метод ввода строки со всеми проверками
+            var textCR = Console.ReadLine();
+
+            // Соответствие закрывающих скобок открывающим
+            var textDict = new Dictionary<char, char>
+            {
+                ['('] = ')',
+                ['['] = ']',
+                ['{'] = '}',
+                ['<'] = '>'
+            };
+
+            // все проверки строки textCR уже сделаны
+            var textStack = new Stack<char>(); int i = 0;
+            foreach (var s in textCR)
+            {
+                if (s == '(' || s == '[') textStack.Push(s);
+                if ((s == ')' || s == ']') && s == textDict[textStack.Peek()]) { textStack.Pop(); Console.WriteLine($"{i} - ok"); }
+                i++;
+            }
+        }
+    }
+}
+
+// Немного поигрался с Console.ReadKey();
+//ConsoleKeyInfo klav;
+//do
+//{
+//    klav = Console.ReadKey();
+//    if (klav.KeyChar == '(' || klav.KeyChar == '[' || klav.KeyChar == ')' || klav.KeyChar == ']')
+//    {
+//        textCR += klav.KeyChar;
+//        continue;
+//    }
+
+//    if (klav.Key != ConsoleKey.Enter)
+//    {
+//        Console.WriteLine(
+//            $"\n\'{klav.KeyChar}\' - Недопустимый символ." +
+//            "\nПродолжайте вводить один из следующих символов: \'(\', \')\', \'[\', \']\'. Затем нажмите клавишу ввода." +
+//            $"\nНа данный момент введено \'{textCR}\'"
+//            );
+//    }
+//}
+//while (klav.Key != ConsoleKey.Enter) ;
