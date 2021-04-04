dfb991f8090b50c594bf7fffee85743e84159a1f
DavidZolotoi <zolotoi77@inbox.ru>
2021-04-05 00:09:08 +0300

    Исправил с учетом замечаний Артема

--- lessons/08/Homework08/Homework08/Program.cs
+++ lessons/08/Homework08/Homework08/Program.cs
@@ -19,32 +19,40 @@ namespace Homework08
             // Метод для ввода строки со всеми проверками, получающий допустимые значения и возвращающий изменённую строку только из допустимых
             var textCR = ReadString("выражение для проверки, содержащее скобки: ", dictSkobky);
 
+            // Вывод
+            if (getResult(textCR, dictSkobky))        // ЕСЛИ вернул true
+                WriteWithColor("Порядок и количество скобок верные.", ConsoleColor.Green);
+            else                                      // ИНАЧЕ вернул false
+                WriteWithColor("Порядок и количество скобок неверные.", ConsoleColor.Red);
+        }
+
+        // Метод - суть программы - проверка выражения на правильность
+        static bool getResult(string textCR, Dictionary<char, char> dictSkobky)
+        {
             // все проверки строки textCR на корректность исходных данных уже сделаны выше.
             var stackSkobky = new Stack<char>();
-            bool bPeek, bKeyValue, bPop, bResult = false;
+            bool getPeek, getKeyValue, getPop, getStackStatus = false;
             char peek, stackPeekDict, stackPop;
             foreach (var s in textCR)
             {
                 if (dictSkobky.ContainsKey(s))           // 1. ЕСЛИ текущий символ относится к ключам (открывающим скобкам)
-                {                                                                                               // ТО
-                    stackSkobky.Push(s);                                                                        // То положить на стек открывающую скобку, ждущую закрытия
-                    bResult = true;                                                                             // И зафиксировать факт того, что в стек положили открывающую скобку
+                {                                                                                                   // ТО
+                    stackSkobky.Push(s);                                                                            // То положить на стек открывающую скобку, ждущую закрытия
+                    getStackStatus = true;                                                                          // И зафиксировать факт того, что в стек положили открывающую скобку
                 }
                 else if (dictSkobky.ContainsValue(s))    // 2. ЕСЛИ текущий символ относится к значениям (закрывающим скобкам)
-                {                                                                                               // ТО
-                    if (stackSkobky.Count == 0) bResult = false;                                                // зафиксировать факт того, что есть закрывающие, а открывающих в стеке нет
-                    bPeek = stackSkobky.TryPeek(out peek);                                                      // Получилось ли получить значение открывающей скобки на стеке? Если нет, занчит - else
-                    bKeyValue = dictSkobky.TryGetValue(peek, out stackPeekDict);                                // Получилось ли найти для неё значение закрывающей скобки из словаря? Если нет, занчит - else
-                    if ((s == stackPeekDict) && bPeek && bKeyValue) bPop = stackSkobky.TryPop(out stackPop);    // Если всё получилось и текущая закрывающая скобка совпадает со значением из словаря. Если нет, занчит - else
+                {                                                                                                   // ТО
+                    if (stackSkobky.Count == 0) getStackStatus = false;                                             // зафиксировать факт того, что есть закрывающие, а открывающих в стеке нет
+                    getPeek = stackSkobky.TryPeek(out peek);                                                        // Получилось ли получить значение открывающей скобки на стеке? Если нет, занчит - else
+                    getKeyValue = dictSkobky.TryGetValue(peek, out stackPeekDict);                                  // Получилось ли найти для неё значение закрывающей скобки из словаря? Если нет, занчит - else
+                    if ((s == stackPeekDict) && getPeek && getKeyValue) getPop = stackSkobky.TryPop(out stackPop);  // Если всё получилось и текущая закрывающая скобка совпадает со значением из словаря. Если нет, занчит - else
                     //Console.WriteLine($"тек.символ {s}, на стеке {peek}, значение из словаря {stackPeekDict}");
                 }
             }
 
-            // Вывод
-            if (bResult && stackSkobky.Count == 0)    // ЕСЛИ значений в стеке не осталось и нет лишних закрывающих скобок, то всё ок
-                WriteWithColor("Порядок и количество скобок верные.", ConsoleColor.Green);
-            else                                      // ИНАЧЕ, либо есть закрывающие, но нет открывающих (bResult=false), либо наоборот (stackSkobky.Count>0), либо и то и другое
-                WriteWithColor("В порядке или количестве скобок есть ошибка.", ConsoleColor.Red);
+            // ЕСЛИ значений в стеке не осталось и нет лишних закрывающих скобок, то всё ок
+            // ИНАЧЕ, либо есть закрывающие, но нет открывающих (bResult=false), либо наоборот (stackSkobky.Count>0), либо и то и другое
+            return getStackStatus && stackSkobky.Count == 0;
         }
 
         // Метод для ввода строки со всеми проверками, получающий допустимые значения и возвращающий изменённую строку только из допустимых
@@ -64,10 +72,19 @@ namespace Homework08
                     // проверка на пустоту
                     if (string.IsNullOrWhiteSpace(text))
                     {
-                        WriteWithColor("Вы не ввели ни одной скобки из перечисленных выше. Пожалуйста, попробуйте еще раз.", ConsoleColor.Red);
+                        WriteWithColor("Вы не ввели пустой текст. Пожалуйста, попробуйте еще раз.", ConsoleColor.Red);
                         continue;
                     }
 
+                    // Проверка на наличие скобок из словаря
+                    bool isHave = false;
+                    foreach (var item in text)
+                        isHave = dict.ContainsKey(item) || dict.ContainsValue(item);
+                    if (!isHave)
+                    {
+                        WriteWithColor("Вы не ввели ни одной скобки из перечисленных выше. Пожалуйста, попробуйте еще раз.", ConsoleColor.Red);
+                        continue;
+                    }
                     return text;
                 }
                 catch (ArgumentOutOfRangeException exception)   //Console.ReadLine()
--- lessons/08/Homework08/Homework08_2/Program.cs
+++ lessons/08/Homework08/Homework08_2/Program.cs
@@ -17,41 +17,58 @@ namespace Homework08_2
             };
 
             // Варианты для проверки
-            //var textCR = "()";              // True
-            //var textCR = "[]()";            // True
-            //var textCR = "[[]()]";          // True
-            //var textCR = "([([])])()[]";    // True
-            //var textCR = "(";               // False
-            //var textCR = "[][)";            // False
-            //var textCR = "[(])";            // False
-            var textCR = "(()[]]";          // False
+            var variant = new string[]
+                {
+                "()",                           // True
+                "[]()",                         // True
+                "[[]()]",                       // True
+                "([([])])()[]",                 // True
+                "(",                            // False
+                "[][)",                         // False
+                "[(])",                         // False
+                "(()[]]",                       // False
+                "0.5*(x*x+[cos{b}*2-5<r-p>])+1" // True
+                };
 
+            // Проверка вариантов в массиве
+            foreach (var item in variant)
+            {
+                Console.WriteLine($"В выражении: {item}");
+                if (getResult(item, dictSkobky))        // ЕСЛИ вернул true
+                    WriteWithColor("Порядок и количество скобок верные.", ConsoleColor.Green);
+                else                                      // ИНАЧЕ вернул false
+                    WriteWithColor("Порядок и количество скобок неверные.", ConsoleColor.Red);
+                Console.WriteLine();
+            }
+        }
+
+        // Метод - суть программы - проверка выражения на правильность
+        static bool getResult(string textCR, Dictionary<char, char> dictSkobky)
+        {
             // все проверки строки textCR на корректность исходных данных уже сделаны выше.
             var stackSkobky = new Stack<char>();
-            bool bPeek, bKeyValue, bPop, bResult = false;
+            bool getPeek, getKeyValue, getPop, getStackStatus = false;
             char peek, stackPeekDict, stackPop;
             foreach (var s in textCR)
             {
                 if (dictSkobky.ContainsKey(s))           // 1. ЕСЛИ текущий символ относится к ключам (открывающим скобкам)
-                {                                                                                               // ТО
-                    stackSkobky.Push(s);                                                                        // То положить на стек открывающую скобку, ждущую закрытия
-                    bResult = true;                                                                             // И зафиксировать факт того, что в стек положили открывающую скобку
+                {                                                                                                   // ТО
+                    stackSkobky.Push(s);                                                                            // То положить на стек открывающую скобку, ждущую закрытия
+                    getStackStatus = true;                                                                          // И зафиксировать факт того, что в стек положили открывающую скобку
                 }
                 else if (dictSkobky.ContainsValue(s))    // 2. ЕСЛИ текущий символ относится к значениям (закрывающим скобкам)
-                {                                                                                               // ТО
-                    if (stackSkobky.Count == 0) bResult = false;                                                // зафиксировать факт того, что есть закрывающие, а открывающих в стеке нет
-                    bPeek = stackSkobky.TryPeek(out peek);                                                      // Получилось ли получить значение открывающей скобки на стеке? Если нет, занчит - else
-                    bKeyValue = dictSkobky.TryGetValue(peek, out stackPeekDict);                                // Получилось ли найти для неё значение закрывающей скобки из словаря? Если нет, занчит - else
-                    if ((s == stackPeekDict) && bPeek && bKeyValue) bPop = stackSkobky.TryPop(out stackPop);    // Если всё получилось и текущая закрывающая скобка совпадает со значением из словаря. Если нет, занчит - else
+                {                                                                                                   // ТО
+                    if (stackSkobky.Count == 0) getStackStatus = false;                                             // зафиксировать факт того, что есть закрывающие, а открывающих в стеке нет
+                    getPeek = stackSkobky.TryPeek(out peek);                                                        // Получилось ли получить значение открывающей скобки на стеке? Если нет, занчит - else
+                    getKeyValue = dictSkobky.TryGetValue(peek, out stackPeekDict);                                  // Получилось ли найти для неё значение закрывающей скобки из словаря? Если нет, занчит - else
+                    if ((s == stackPeekDict) && getPeek && getKeyValue) getPop = stackSkobky.TryPop(out stackPop);  // Если всё получилось и текущая закрывающая скобка совпадает со значением из словаря. Если нет, занчит - else
                     //Console.WriteLine($"тек.символ {s}, на стеке {peek}, значение из словаря {stackPeekDict}");
                 }
             }
 
-            // Вывод
-            if (bResult && stackSkobky.Count == 0)    // ЕСЛИ значений в стеке не осталось и нет лишних закрывающих скобок, то всё ок
-                WriteWithColor("Порядок и количество скобок верные.", ConsoleColor.Green);
-            else                                      // ИНАЧЕ, либо есть закрывающие, но нет открывающих (bResult=false), либо наоборот (stackSkobky.Count>0), либо и то и другое
-                WriteWithColor("В порядке или количестве скобок есть ошибка.", ConsoleColor.Red);
+            // ЕСЛИ значений в стеке не осталось и нет лишних закрывающих скобок, то всё ок
+            // ИНАЧЕ, либо есть закрывающие, но нет открывающих (bResult=false), либо наоборот (stackSkobky.Count>0), либо и то и другое
+            return getStackStatus && stackSkobky.Count == 0;
         }
 
         // Метод вывода сообщения в цвете
