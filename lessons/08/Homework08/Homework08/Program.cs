7c551d09be12483d07c828d334e8c80f7c7bca38
DavidZolotoi <zolotoi77@inbox.ru>
2021-04-04 04:30:18 +0300

    Доделал ДЗ № 08

--- lessons/08/Homework08/Homework08/Program.cs
+++ lessons/08/Homework08/Homework08/Program.cs
@@ -7,11 +7,8 @@ namespace Homework08
     {
         static void Main(string[] args)
         {
-            // Метод ввода строки со всеми проверками
-            var textCR = Console.ReadLine();
-
-            // Соответствие закрывающих скобок открывающим
-            var textDict = new Dictionary<char, char>
+            // Соответствие закрывающих скобок (значений) открывающим (ключам)
+            var dictSkobky = new Dictionary<char, char>
             {
                 ['('] = ')',
                 ['['] = ']',
@@ -19,15 +16,97 @@ namespace Homework08
                 ['<'] = '>'
             };
 
-            // все проверки строки textCR уже сделаны
-            var textStack = new Stack<char>(); int i = 0;
+            // получение строк с допустимыми символами ключей, значений и общей
+            string dictKeys="", dictValues="", dictKeysValues;
+            foreach (var (key, value) in dictSkobky)
+            {
+                dictKeys += key.ToString();
+                dictValues += value.ToString();
+            }
+            dictKeysValues = dictKeys + dictValues;
+
+            // Метод для ввода строки со всеми проверками, получающий допустимые значения и возвращающий изменённую строку только из допустимых
+            var textCR = ReadString("выражение для проверки, содержащее скобки: " + dictKeysValues + "\n", dictKeysValues);
+
+        //    textCR = "()";              // True
+        //    textCR = "[]()";            // True
+        //    textCR = "[[]()]";          // True
+        //    textCR = "([([])])()[]";    // True
+        //    textCR = "(";               // False
+        //    textCR = "[][)";            // False
+        //    textCR = "[(])";            // False
+        //    textCR = "(()[]]";          // False
+
+            // все проверки строки textCR на корректность исходных данных уже сделаны выше. Сейчас строка только из скобок
+            var stackSkobky = new Stack<char>();
+            bool bPeek, bKeyValue, bPop, bResult=false;
+            char peek, stackPeekDict, stackPop;
             foreach (var s in textCR)
             {
-                if (s == '(' || s == '[') textStack.Push(s);
-                if ((s == ')' || s == ']') && s == textDict[textStack.Peek()]) { textStack.Pop(); Console.WriteLine($"{i} - ok"); }
-                i++;
+                if (dictKeys.Contains(s))           // 1. ЕСЛИ текущий символ относится к ключам (открывающим скобкам)
+                {
+                    stackSkobky.Push(s);                                                                        // То положить на стек открывающую скобку
+                    bResult = true;                                                                             // И зафиксировать факт того, что в стек когда-то что-то было положено
+                }
+                else if (dictValues.Contains(s))    // 2. ЕСЛИ текущий символ относится к значениям (закрывающим скобкам)
+                {                                                                                               // ТО
+                    bPeek = stackSkobky.TryPeek(out peek);                                                      // Получилось ли получить значение открывающей скобки на стеке? Если нет, занчит - else
+                    bKeyValue = dictSkobky.TryGetValue(peek, out stackPeekDict);                                // Получилось ли найти для неё значение закрывающей скобки из словаря? Если нет, занчит - else
+                    if ((s == stackPeekDict) && bPeek && bKeyValue) bPop = stackSkobky.TryPop(out stackPop);    // Если всё получилось и текущая закрывающая скобка совпадает со значением из словаря. Если нет, занчит - else
+                    //Console.WriteLine($"тек.символ {s}, на стеке {peek}, значение из словаря {stackPeekDict}");
+                }
+            }
+
+            // Вывод
+            if (bResult && stackSkobky.Count == 0)  // ЕСЛИ значения были в стеке, но теперь их нет, значит всё ок
+                WriteWithColor("Порядок и количество скобок верные.", ConsoleColor.Green);
+            else
+                WriteWithColor("В порядке или количестве скобок есть ошибка.", ConsoleColor.Red);
+        }
+
+        // Метод ввода строки со всеми проверками, преобразованиями и возврата строки, содержащей только скобки
+        static string ReadString(string name, string keysValues)
+        {
+            string text;
+            for (; ; )
+            {
+                try
+                {
+                    Console.Write($"Введите {name}");
+                    text = Console.ReadLine();
+
+                    // сбор строки из введенной, содержащей только скобки (только интересующие символы = сумма символов ключей и значений данного словаря)
+                    string skobky = "";
+                    foreach (char sText in text)
+                    {
+                        if (keysValues.Contains(sText)) skobky += sText;
+                    }
+
+                    // проверка на пустоту
+                    if (string.IsNullOrWhiteSpace(skobky))
+                    {
+                        WriteWithColor("Вы не ввели ни одной скобки из перечисленных выше. Пожалуйста, попробуйте еще раз.", ConsoleColor.Red);
+                        continue;
+                    }
+
+                    return skobky;
+                }
+                catch (ArgumentOutOfRangeException exception)   //Console.ReadLine()
+                {
+                    WriteWithColor("Слишком большое количество символов\n" + exception.Message, ConsoleColor.Red);
+                }
             }
         }
+
+        // Метод вывода сообщения в цвете
+        static void WriteWithColor(string message, ConsoleColor color)
+        {
+            var restoreColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = restoreColor;
+        }
+
     }
 }
 
