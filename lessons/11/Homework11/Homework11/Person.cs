a66eeada4ce074ef422a8dd575946c6350f4e86a
David Zolotoi <zolotoi77@inbox.ru>
2021-04-09 19:19:47 +0300

    Сделал домашнее задание № 11

---
+++ lessons/11/Homework11/Homework11/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework11
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int AgePlus { get => Age + 4; }
+        public string Info { get => $"Name: {Name}, age in 4 years: {AgePlus}."; }
+    }
+}
