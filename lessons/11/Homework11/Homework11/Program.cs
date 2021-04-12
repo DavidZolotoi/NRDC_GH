9e098678875c3f1a353bca0e9fa2531577f3b171
DavidZolotoi <zolotoi77@inbox.ru>
2021-04-13 00:49:46 +0300

    начал дз 11

---
+++ lessons/11/Homework11/Homework11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework11
+{
+    class ReminderItem
+    {
+        // Свойства
+        public DateTimeOffset AlarmDate { get; set; }   // ДАТА БУДИЛЬНИКА
+        public string AlarmMessage { get; set; }        // Сообщение будтльника
+        public TimeSpan TimeToAlarm;    // => текущее время минус AlarmDate
+        public bool IsOutdated;    //=> true, если TimeToAlarm больше либо равно 0
+
+        // Методы
+        public ReminderItem()       // конструктор
+        {
+            // значение AlarmDate
+            AlarmDate = new DateTimeOffset(2021, 04, 13, 15, 30, 00, TimeToAlarm);      //?
+            // AlarmMessage
+            AlarmMessage = "Пора на работу";
+        }
+        public static void WriteProperties() { }    //выводить на экран все
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
