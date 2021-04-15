134cc5c340a3218f12dd4ef8af4dd244e90d8f33
DavidZolotoi <zolotoi77@inbox.ru>
2021-04-15 21:57:49 +0300

    Класссаная 13

---
+++ lessons/13/Classwork13/Classwork13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Classwork13
+{
+    abstract class FlyMachine
+    {
+        public int MaxHeight { get; private set; }
+        public int CurrentHeight { get; private set; }
+        public FlyMachine(int maxHeight, byte bladesCount)
+        {
+            MaxHeight = maxHeight;
+        }
+        protected void TakeUpper(int delta)
+        {
+            if (delta <= 0) throw new ArgumentOutOfRangeException();
+            if (CurrentHeight + delta > MaxHeight) delta = MaxHeight - CurrentHeight;
+            CurrentHeight += delta;
+        }
+        protected void TakeLower(int delta)
+        {
+            if (delta <= 0) throw new ArgumentOutOfRangeException();
+            if (CurrentHeight - delta < 0) throw new InvalidOperationException("Crash");
+            CurrentHeight -= delta;
+        }
+    }
+
+    class Helicopter:FlyMachine
+    {
+        public byte BladesCount { get; private set; }
+        public Helicopter(int maxHeight, byte bladesCount):base(maxHeight, bladesCount)
+        {
+            // НЕДОДЕЛАЛ
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
