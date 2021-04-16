b3694fe852c529badbbc2853656242f2cea68046
David Zolotoi <zolotoi77@inbox.ru>
2021-04-16 17:39:26 +0300

    Добавил дублирование проверки строки в конструктор Добавил значение по умолчанию для аргумента Вынес класс будильника в отдельный файл В Main сделал массив будильников и прогнал в цикле

--- lessons/11/Homework11/Homework11/Program.cs
+++ lessons/11/Homework11/Homework11/Program.cs
@@ -2,73 +2,44 @@
 
 namespace Homework11
 {
-    class ReminderItem
-    {
-        // Свойства
-        public DateTimeOffset AlarmDate { get; set; }                           // ДАТА БУДИЛЬНИКА
-        public string AlarmMessage { get; set; }                                // Сообщение будильника
-        public TimeSpan TimeToAlarm => DateTimeOffset.UtcNow - AlarmDate;       // разница от настоящего времени
-        public bool IsOutdated => (TimeToAlarm.TotalSeconds >= 0);              // прошел?
-
-        // Методы
-        public ReminderItem(DateTimeOffset alarmDate, string alarmMessage)
-        {
-            AlarmDate = alarmDate;          // все проверки были в методе ввода с бесконечным циклом и ловлей исключений
-            AlarmMessage = alarmMessage;    // все проверки были в методе ввода с бесконечным циклом и ловлей исключений 
-        }
-
-        public void WriteProperties()    // метод вывода на экран все
-        {
-            Console.WriteLine
-                (
-                $"AlarmDate: {AlarmDate},\n" +
-                $"AlarmMessage: {AlarmMessage},\n" +
-                $"TimeToAlarm: {TimeToAlarm},\n" +
-                $"IsOutdated: {IsOutdated}"
-                );
-        }
-}
-
 
     class Program
     {
         static void Main(string[] args)
         {
-           ReminderItem ri1 = new ReminderItem
-                (
-                ReadDateTime("Введите дату (день/месяц/год), время (часы:минуты:секунды AM/PM) и разницу часовых поясов через пробел (знак ч:м): "),
-                ReadTitle("Введите название будильника: ")
-                );
-            ri1.WriteProperties();
 
-            ReminderItem ri2 = new ReminderItem
-                (
-                ReadDateTime("Введите дату (день/месяц/год), время (часы:минуты:секунды AM/PM) и разницу часовых поясов через пробел (знак ч:м): "),
-                ReadTitle("Введите название будильника: ")
-                );
-            ri2.WriteProperties();
+            var reminderItems = new ReminderItem[2];
+            for (int i = 0; i < reminderItems.Length; i++)
+            {
+                reminderItems[i] = new ReminderItem
+                    (
+                    ReadDateTime(),
+                    ReadTitle()
+                    );
+                reminderItems[i].WriteProperties();
+            }
+
         }
 
 
         // Метод для ввода даты (день/месяц/год), времени (часы:минуты:секунды AM/PM) и разницы часовых поясов через пробел (± ч:м) со всеми проверками
-        static DateTimeOffset ReadDateTime(string name)
+        static DateTimeOffset ReadDateTime()
         {
-            string text;
             for (; ; )
             {
                 try
                 {
-                    Console.WriteLine(name);
-                    text = Console.ReadLine();
+                    Console.WriteLine("Введите дату (день/месяц/год), время (часы:минуты:секунды AM/PM) и разницу часовых поясов через пробел (знак ч:м): ");
+                    string readDateTimeString = Console.ReadLine();
                     // проверка на пустоту
-                    if (string.IsNullOrWhiteSpace(text))
+                    if (string.IsNullOrWhiteSpace(readDateTimeString))
                     {
                         Console.WriteLine("Вы ввели пустой текст. Пожалуйста, попробуйте еще раз.");
                         continue;
                     }
                     else
                     {
-                        return DateTimeOffset.Parse(text);
+                        return DateTimeOffset.Parse(readDateTimeString);
                     }
                 }
                 catch (ArgumentOutOfRangeException exception)   //Console.ReadLine()
@@ -87,14 +58,14 @@ namespace Homework11
         }
 
         // Метод для ввода строки со всеми проверками
-        static string ReadTitle(string name)
+        static string ReadTitle()
         {
             string text;
             for (; ; )
             {
                 try
                 {
-                    Console.Write($"{name}");
+                    Console.Write($"Введите название будильника: ");
                     text = Console.ReadLine();
 
                     // проверка на пустоту
@@ -112,6 +83,5 @@ namespace Homework11
                 }
             }
         }
-
     }
 }
---
+++ lessons/11/Homework11/Homework11/ReminderItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework11
+{
+    class ReminderItem
+    {
+        // Свойства
+        public DateTimeOffset AlarmDate { get; set; }                           // ДАТА БУДИЛЬНИКА
+        public string AlarmMessage { get; set; }                                // Сообщение будильника
+        public TimeSpan TimeToAlarm => DateTimeOffset.UtcNow - AlarmDate;       // разница от настоящего времени
+        public bool IsOutdated => (TimeToAlarm.TotalSeconds >= 0);              // прошел?
+
+        public ReminderItem(DateTimeOffset alarmDate, string alarmMessage="Будильник")
+        {
+            AlarmDate = alarmDate;
+            if (string.IsNullOrWhiteSpace(alarmMessage))
+                Console.WriteLine($"Что-то не так с наименованием будильника. Будет записано значение по умолчанию: \"{alarmMessage}\"");
+                AlarmMessage = alarmMessage;
+        }
+
+        public void WriteProperties()    // метод вывода на экран все
+        {
+            Console.WriteLine
+                (
+                $"AlarmDate: {AlarmDate},\n" +
+                $"AlarmMessage: {AlarmMessage},\n" +
+                $"TimeToAlarm: {TimeToAlarm},\n" +
+                $"IsOutdated: {IsOutdated}"
+                );
+        }
+    }
+}
