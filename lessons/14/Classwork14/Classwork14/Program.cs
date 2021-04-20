e5bc673abcb365cf61662b0c4a3c3f28723f574b
DavidZolotoi <zolotoi77@inbox.ru>
2021-04-20 22:35:53 +0300

    классаная работа № 14

---
+++ lessons/14/Classwork14/Classwork14/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Classwork14
+{
+    class ErrorList:IDisposable, IEnumerable<string>
+    {
+        public string Category { get;}
+        private List<string> Errors { get; set; }
+        public ErrorList(string category, string error)
+        {
+            Category = category;
+            Errors = new List<string>();
+            Errors.Add(error);
+        }
+        public void Dispose()
+        {
+            Errors.Clear();
+            Errors = null;
+            //throw new NotImplementedException();
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return Errors.GetEnumerator();
+            // throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }   // обратная связь (для использования старого)
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var er1 = new ErrorList("dfv", "ewrf");
+            er1.Errors.Add("dvqdcqdc");
+            er1.Errors.Add("dfsfsff");
+
+            foreach (var item in er1.Errors)
+            {
+                Console.WriteLine($"Категория: {item}");
+            }
+
+        }
+    }
+}
