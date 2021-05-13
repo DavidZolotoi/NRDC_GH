9282524a7f53b4f78e0e4ffdd84f6a996b19223e
DavidZolotoi <zolotoi77@inbox.ru>
2021-05-13 21:59:35 +0300

    16 занятие

---
+++ lessons/16/classwork16/classwork16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace classwork16
+{
+    public sealed class Circle
+    {
+        private double _radius;
+        public Circle (double radius)
+        {
+            _radius = radius;
+        }
+        public double Calculate(Func<double, double> operation)
+        {
+            return operation(_radius);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(2);
+            var d = c.Calculate((r) => 2*r);
+            var s = c.Calculate((r) => Math.PI*r*r);
+            var l = c.Calculate((r) => 2*Math.PI*r);
+        }
+    }
+}
